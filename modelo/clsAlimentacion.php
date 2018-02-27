32e5d67f268baaedf4286aca1a733fa6d2749726
Familia Granados Caceres <fliagranc@iMac-de-Familia.local>
2018-02-27 08:01:23 -0500

    Correcion preprogramacion

--- modelo/clsAlimentacion.php
+++ modelo/clsAlimentacion.php
@@ -25,10 +25,8 @@ class clsAlimentacion {
         echo json_encode($array);
     }
 
-
   public function agregarAlimentacioGeneral($param) 
-    {
-        
+    {  
         extract($param);
 		$array = array();
 		$rs = null;		
@@ -212,6 +210,102 @@ class clsAlimentacion {
             } //print_r($array);
         //}
         echo json_encode($array);
+    }
+
+    //genera informe en archivo excel alimentación
+    public function consultarReporteAlimentacion ($param){
+        define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+        date_default_timezone_set('Europe/London');
+        /** PHPExcel_IOFactory */
+         
+        require_once dirname(__FILE__) . '/../includes/PHPExcel/PHPExcel/IOFactory.php';
+
+        /*
+         * To change this template, choose Tools | Templates
+         * and open the template in the editor.
+         */
+
+        $cacheMethod = PHPExcel_CachedObjectStorageFactory:: cache_to_phpTemp;
+        $cacheSettings = array('memoryCacheSize ' => '256MB');
+        PHPExcel_Settings::setCacheStorageMethod($cacheMethod, $cacheSettings);
+
+        $sheetname = 'Informe'; 
+        $objReader = PHPExcel_IOFactory::createReader('Excel5');
+        $objReader->setLoadSheetsOnly($sheetname);
+        //$objReader->setLoadAllSheets();
+        $objPHPExcel = $objReader->load("../includes/PHPExcel/PHPExcel/Templates/template.xls");
+        
+        extract($param);
+        $data = array('error'=>0,'mensaje'=>'','html'=>'');
+
+        $objPHPExcel = $objReader->load("../includes/PHPExcel/PHPExcel/Templates/templateReporte.xls");
+        $objPHPExcel->getActiveSheet()->setCellValue('D2', PHPExcel_Shared_Date::PHPToExcel(time()));
+        
+        $objPHPExcel->getActiveSheet()->setCellValue('C3', 'INFORME AlIMENTACIÓN');
+        //Array de Datos Columnas
+        $dataColumnasDatos = array('Salón','Curso', 'Modulo', 'Estado', 'Sesion', 'Fecha',  'Cantidad', 'Refrigerio');                    
+        
+        $baseRowDatos = 5;
+        $columnDatos=0;
+        foreach($dataColumnasDatos as $dataRow) {
+            $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($columnDatos, $baseRowDatos, $dataRow);     
+            $columnDatos++;                    
+        }   
+
+    $baseRow = 6;
+    $columnRow= 0;
+
+    $conexion->getPDO()->query("SET NAMES 'utf8'");
+    $sql = "CALL SPREPORTEALIMENTACIONGENERAL('$fechai','$fechaf');";
+    $rs=null;
+    // $sql = "CALL SPREPORTEASISTENCIADETALLE1('".$fechaInicial."','".$fechaFinal."',".$convocatoria.");";
+    // //print_r($sql);
+    if ($rs = $conexion->getPDO()->query($sql)) {
+    if ($filas = $rs->fetchAll(PDO::FETCH_ASSOC)) {
+    $data['mensaje'] = 1;
+    //print_r($filas);
+    foreach ($filas as  $r =>$fila) {
+        //$array[] = $fila;
+        $columnRow=0;
+        $row = $baseRow + $r;
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($columnRow=$columnRow,   $row, $fila['Salon']);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($columnRow=$columnRow+1, $row, $fila['Curso']);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($columnRow=$columnRow+1, $row, $fila['Modulo']);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($columnRow=$columnRow+1, $row, $fila['Estado']);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($columnRow=$columnRow+1, $row, $fila['Sesion']);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($columnRow=$columnRow+1, $row, $fila['Fecha']);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($columnRow=$columnRow+1, $row, $fila['Cantidad']);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow($columnRow=$columnRow+1, $row, $fila['Refrigerio']);    
+        }
+        }else{
+             $data['error']=2;
+        }
+        }else {
+             $data['error'] = 0;
+             print_r($conexion->getPDO()->errorInfo()); die();
+        } 
+            
+        
+        $styleArray = array(
+              'borders' => array(
+                  'allborders' => array(
+                      'style' => PHPExcel_Style_Border::BORDER_THIN
+                  )
+              )
+          );
+         $objPHPExcel->getDefaultStyle()->applyFromArray($styleArray);
+        
+        //foreach externo
+         //$objPHPExcel->getActiveSheet()->removeRow($baseRow-1,1);
+         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+        
+         $FechaMod=strtotime("now");
+         $filename = '../tmp/reporteAlimentacion/reporteAlimentacion_'.$FechaMod.'.xls';
+         $objWriter->save(str_replace('.php', '.xls', $filename));
+         $data['html']=$filename;
+
+            echo json_encode($data);
+
     }
 
 }
